53465f1fb8ed2ef86563ec6be0458e8ccb8789f0
theprince1701 <liam.mcelwain@ontariotechu.net>
2022-03-31 18:50:58 -0400

    recharge thingy

--- Assets/Scripts/FlashlightRechargingStation.cs
+++ Assets/Scripts/FlashlightRechargingStation.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class FlashlightRechargingStation : MonoBehaviour
+public class FlashlightRechargingStation : InteractableObject
 {
+    public override void ButtonInteract()
+    {
+
+    }
+
+    public override void HoverInteract()
+    {
+    }
+
+    public override void StopHoverInteract()
+    {
+    }
+
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (CheckCollision(collision))
@@ -14,6 +28,7 @@ public class FlashlightRechargingStation : MonoBehaviour
         if (CheckCollision(collision))
             GameEvents.OnToggleRechargingStation?.Invoke(false);
     }
+
 
     private bool CheckCollision(Collider2D collision)
     {
